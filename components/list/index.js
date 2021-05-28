cbdf312154ed513ef1954e598b5b7e70334ea03a
YutLee <664009360@qq.com>
2021-05-28 18:59:31 +0800

    新增列表组件

---
+++ components/list/index.js
@@ -0,0 +1,36 @@
+import BaseComponent from '../../helpers/BaseComponent'
+
+BaseComponent({
+  properties: {
+    url: {
+      type: String,
+      value: ''
+    },
+    label: {
+      type: String,
+      value: ''
+    },
+    desc: {
+      type: String,
+      value: ''
+    },
+    value: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
