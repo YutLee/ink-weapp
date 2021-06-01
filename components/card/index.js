b9afe91a14bb88c5f0a9176ceea3ebd910d255c0
YutLee <664009360@qq.com>
2021-06-01 16:39:59 +0800

    新增卡片组件

---
+++ components/card/index.js
@@ -0,0 +1,50 @@
+Component({
+  properties: {
+    full: {
+      type: Boolean,
+      value: false
+    },
+    thumb: {
+      type: String,
+      value: ''
+    },
+    icon: {
+      type: String,
+      value: ''
+    },
+    url: {
+      type: String,
+      value: ''
+    },
+    title: {
+      type: String,
+      value: ''
+    },
+    extra: {
+      type: String,
+      value: ''
+    },
+    outline: {
+      type: Boolean,
+      value: false
+    },
+    arrow: {
+      type: Boolean,
+      value: false
+    },
+    disabled: {
+      type: Boolean,
+      value: false
+    },
+    footer: {
+      type: String,
+      value: ''
+    }
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+})
