37df97c8d4ebf2d1477feee81a008430376453ae
YutLee <664009360@qq.com>
2021-06-02 10:51:39 +0800

    导航栏组件

--- components/nav-bar/index.js
+++ components/nav-bar/index.js
@@ -1,3 +1,18 @@
-Component({
-  
+import BaseComponent from '../../helpers/BaseComponent'
+
+BaseComponent({
+  properties: {
+    backIcon: {
+      type: String,
+      value: 'xiangzuo1'
+    },
+    statusBar: {
+      type: Boolean,
+      value: true
+    },
+    outline: {
+      type: Boolean,
+      value: true
+    }
+  }
 })
